11cc16752ba08a79e8fad995bcbba24a253a63ba
yufengg <g.yufeng@gmail.com>
2013-11-24 15:56:29 -0500

    Update account_type_table_insert.sql

--- sql/inserts/account_type_table_insert.sql
+++ sql/inserts/account_type_table_insert.sql
@@ -3,4 +3,13 @@ INSERT INTO Account_type_table VALUES (account_type(7, 'Liming Zhang', '2007-03-
 INSERT INTO Account_type_table VALUES (account_type(8, 'Ningning Xia', '2004-08-21'));
 INSERT INTO Account_type_table VALUES (account_type(9, 'Yuedong Huang', '2011-05-07'));
 INSERT INTO Account_type_table VALUES (account_type(10, 'Zhiling Wan', '2013-10-12'));
+
+INSERT INTO Account_type_table VALUES (account_type(1, 'Alex Biliris', '2004-05-13'));
+INSERT INTO Account_type_table VALUES (account_type(2, 'Bohong Zhao', '2005-03-23'));
+INSERT INTO Account_type_table VALUES (account_type(3, 'Harshil Gandhi', '2003-07-31'));
+INSERT INTO Account_type_table VALUES (account_type(4, 'Jason Lee', '2004-04-11'));
+INSERT INTO Account_type_table VALUES (account_type(5, 'Yufeng Guo', '2001-11-06'));
+INSERT INTO Account_type_table VALUES (account_type(11, 'Ash Ketchum', '1997-04-01'));
+
+
 COMMIT;
