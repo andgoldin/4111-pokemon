0e9089568d781dd99dcb782d1ad803526ed5599c
Andrew Goldin <andgoldin@gmail.com>
2013-11-24 15:53:13 -0500

    Create account_type_table_insert.sql

---
+++ sql/inserts/account_type_table_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO Account_type_table VALUES (account_type(6, 'Andrew Goldin', '1998-09-30'));
+INSERT INTO Account_type_table VALUES (account_type(7, 'Liming Zhang', '2007-03-14'));
+INSERT INTO Account_type_table VALUES (account_type(8, 'Ningning Xia', '2004-08-21'));
+INSERT INTO Account_type_table VALUES (account_type(9, 'Yuedong Huang', '2011-05-07'));
+INSERT INTO Account_type_table VALUES (account_type(10, 'Zhiling Wan', '2013-10-12'));
+COMMIT;
